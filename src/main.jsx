2d2a6c6ec5656667dafab4f9a5e8009df29002e1
CodeXSavage6s <iamsavage113377@gmail.com>
2026-02-26 16:06:08 +0100

    PWA setup complete

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import PageProvider from './components/PageProvider'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')).render(
+  <StrictMode>
   <BrowserRouter>
     <PageProvider>
       <App />
     </PageProvider>
   </BrowserRouter>
+  </StrictMode>
 )
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(), // This must be inside the square brackets
+     VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Savage Dashboard',
+        short_name: 'Savage',
+        description: 'Savage React Dashboard App',
+        theme_color: '#0f172a',
+        background_color: '#0f172a',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 })
